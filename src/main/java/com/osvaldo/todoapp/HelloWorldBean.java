43abf925ee6bf413528661d57a8ae099c9deaa94
OsvaldoSalomon <osvaldo.salo21@gmail.com>
2020-06-27 15:12:08 -0400

    Made some get Requests just to test the project, it worked fine

---
+++ src/main/java/com/osvaldo/todoapp/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package com.osvaldo.todoapp;
+
+public class HelloWorldBean {
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
